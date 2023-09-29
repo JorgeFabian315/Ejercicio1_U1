594c15695b5ad05dc6c8079fb2d42dedcb6ffe49
Fabian315 <jfabi@FABIAN>
2023-09-28 22:50:16 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_U1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1_U1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MiPerfil()
+        {
+            return View();
+        }
+    }
+}
